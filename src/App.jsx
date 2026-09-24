5dce46af19b54d4e444937a689ea50efc058577c
Matt Pahuta <mhpahuta@gmail.com>
2026-09-24 14:26:38 -0700

    Add TextStats and derived vals to App, wrapper styles update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import styles from "./App.module.css";
 
 import TextInput from "./components/TextInput/TextInput";
+import TextStats from "./components/TextStats/TextStats";
+
+import {
+  countCharacters,
+  countWords,
+  countSentences,
+} from "./utils/textUtils";
 
 function App() {
-  const [text, setText] = useState("");
-
+  const [text, setText] = useState(""); // primary state
+  // state change handler
   function handleTextChange(event) {
     setText(event.target.value);
   }
+  // derived values from state
+  const characterCount = countCharacters(text);
+  const wordCount = countWords(text);
+  const sentenceCount = countSentences(text);
+
 
   return (
     <div className={styles.main}>
@@ -16,6 +28,11 @@ function App() {
         Analyze your text in real-time.
       </h1>
       <TextInput value={text} onChange={handleTextChange} />
+      <TextStats
+        characterCount={characterCount}
+        wordCount={wordCount}
+        sentenceCount={sentenceCount}
+      />
     </div>
   );
 }
