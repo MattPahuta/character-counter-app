fe434c63040157f36fba5e6075e258179ed86b7c
Matt Pahuta <mhpahuta@gmail.com>
2026-09-24 14:15:08 -0700

    Add TextStats component with initial styles module, styles

---
+++ src/components/TextStats/TextStats.jsx
@@ -0,0 +1,28 @@
+import styles from "./TextStats.module.css";
+
+function TextStats( { characterCount, wordCount, sentenceCount }) {
+  return (
+    <section className={styles.statsWrapper}>
+      <div className={styles.statBox}>
+        <p className={styles.stat}>
+          <span className={styles.value}>{characterCount}</span>
+          <span className={styles.label}>Total Characters</span>
+        </p>
+      </div>
+      <div className={styles.statBox}>
+        <p className={styles.stat}>
+          <span className={styles.value}>{wordCount}</span>
+          <span className={styles.label}>Word Count</span>
+        </p>
+      </div>
+      <div className={styles.statBox}>
+        <p className={styles.stat}>
+          <span className={styles.value}>{sentenceCount}</span>
+          <span className={styles.label}>Sentence Count</span>
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default TextStats;
